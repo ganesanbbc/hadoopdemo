100efba7c707cebef96c5c48cef56b6d68817de9
ganesanbbc <ganesan.ns@cognizant.com>
2016-10-22 16:57:19 +0530

    Textformat Sample

--- Hadaoop3/src/org/samples/mapreduce/training/patientrxMR.java
+++ Hadaoop3/src/org/samples/mapreduce/training/patientrxMR.java
@@ -1,6 +1,8 @@
 package org.samples.mapreduce.training;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -9,14 +11,19 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 //import org.samples.mapreduce.training.counters.USERCOUNTER;
 
-public class textinputformat {
+public class patientrxMR {
 
 	public static class MapDemohadoop extends
 			Mapper<LongWritable, Text, Text, IntWritable> {
@@ -37,7 +44,7 @@ public class textinputformat {
 		}
 	}
 
-	public static class Reducesenthil extends
+	public static class Reducedd extends
 			Reducer<Text, IntWritable, Text, IntWritable> {
 
 		// setup, reduce, run, cleanup
@@ -61,30 +68,25 @@ public class textinputformat {
 		Configuration conf = new Configuration();
 
 		conf.set("mapred.job.tracker", "hdfs://localhost:50001");
-		
 //		conf.set("DrugName", args[3]);
 		Job job = new Job(conf, "Drug Amount Spent");
 
 		job.setJarByClass(patientrxMR.class); // class conmtains mapper and
 												// reducer class
 		
-//		job.setMapOutputKeyClass(Text.class); // map output key class
-//		job.setMapOutputValueClass(IntWritable.class);// map output value class
-		
-		
-//		job.setOutputKeyClass(Text.class); // output key type in reducer
-//		job.setOutputValueClass(IntWritable.class);// output value type in
+		job.setMapOutputKeyClass(Text.class); // map output key class
+		job.setMapOutputValueClass(IntWritable.class);// map output value class
+		job.setOutputKeyClass(Text.class); // output key type in reducer
+		job.setOutputValueClass(IntWritable.class);// output value type in
 													// reducer
 
-//		job.setMapperClass(MapDemohadoop.class);
-//		job.setReducerClass(Reducesenthil.class);
+		job.setMapperClass(MapDemohadoop.class);
+		job.setReducerClass(Reducedd.class);
 		job.setNumReduceTasks(1);
 		job.setInputFormatClass(TextInputFormat.class); // default -- inputkey
 														// type -- longwritable
 														// : valuetype is text
 		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
